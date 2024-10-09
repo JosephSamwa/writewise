55e6ba3c2b934910ac661da4866b766782c78f9d
JOSEPH SAMWA <166645581+JosephSamwa@users.noreply.github.com>
2024-10-09 17:27:01 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, UserProfile
-from payments.models import Payment
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
